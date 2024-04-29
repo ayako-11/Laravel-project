ccd0458a1e0395d6bbe8f57a9330d8b03b732de1
AyakoSugiyama <a.sugiyama@balocco.info>
2024-04-30 08:55:31 +0900

    画面作成：TODO登録

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,10 +8,33 @@ use Illuminate\Http\Request;
 class SaveController extends Controller
 {
     /**
-     * Handle the incoming request and return the view.
+     * Display a listing of the resource.
      */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
         return view('save');
     }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // 入力内容のチェック
+        // ルールに一致しない入力の場合は、自動的に入力画面を表示させる
+        $request->validate([
+            'title' => 'required|max:120',
+            'detail' => 'required|max:1000',
+            'start_date' => 'required|date|after_or_equal:today',
+            'end_date' => 'required|date|after_or_equal:start_date',
+            'priority' => 'required|in:urgent,high,low',
+        ]);
+
+        return view('save');
+    }
+
 }
--- resources/views/save.blade.php
+++ resources/views/save.blade.php
@@ -6,36 +6,60 @@
     <title>TODOの新規登録</title>
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap 5 Datepicker CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-datepicker@1.9.0/dist/css/bootstrap-datepicker.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-4">
         <h1>TODOの新規登録</h1>
+    <!-- errors.blade.phpを読み込む -->
+            @include('errors')
         <form>
             <div class="mb-3">
                 <label for="title">件名</label>
-                <input type="text" class="form-control" id="title" placeholder="入力フォーム">
+                <textarea name="title" class="form-control" id="titleTextarea" rows="3" placeholder="入力フォーム"></textarea>
             </div>
             <div class="mb-3">
                 <label for="description">詳細</label>
-                <textarea class="form-control" id="description" rows="3" placeholder="入力フォーム"></textarea>
+                <textarea name="detail" class="form-control" id="detailTextarea" rows="3" placeholder="入力フォーム"></textarea>
             </div>
-            <div class="mb-3">
-                <label for="due_date">期限</label>
-                <input type="text" class="form-control" id="due_date" placeholder="年/月/日~年/月/日">
-            </div>
-            <div class="mb-3">
-                <label for="priority">優先度</label>
-                <select class="form-select" id="priority">
-                    <option value="urgent">緊急</option>
-                    <option value="high">高</option>
-                    <option value="low">低</option>
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary">登録</button>
-        </form>
-    </div>
+            <div class="mb-3 row">
+                <div class="col">
+                                <label for="start_date">期限 開始</label>
+                                <input type="text" class="form-control" id="start_date" name="start_date" placeholder="年/月/日">
+                            </div>
+                            <div class="col">
+                                <label for="end_date">期限 終了</label>
+                                <input type="text" class="form-control" id="end_date" name="end_date" placeholder="年/月/日">
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="priority">優先度</label>
+                            <select class="form-select" id="priority">
+                                <option value="urgent">緊急</option>
+                                <option value="high">高</option>
+                                <option value="low">低</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary">登録</button>
+                    </form>
+                </div>
 
-    <!-- Bootstrap 5 JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+                <!-- Bootstrap 5 JS -->
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+                <!-- jQuery (Necessary for Bootstrap's JavaScript plugins) -->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                <!-- Bootstrap 5 Datepicker JS -->
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.min.js"></script>
+                <script>
+                    // 期限をカレンダーで選択できるようにする
+                    $(document).ready(function(){
+                        $('#start_date, #end_date').datepicker({
+                            format: 'yyyy/mm/dd', // 期限のフォーマット
+                            autoclose: true, // カレンダーをクリックした後、自動的に閉じる
+                            todayHighlight: true // 今日の日付をハイライト表示する
+                        });
+                    });
+                </script>
+            </body>
+            </html>
